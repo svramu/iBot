15032a932e10ebf01069f955e3801f05d5bbe459
Senthil.sb <senthil.sb@toasttab.com>
2023-01-13 14:43:12 +0530

    Create runtest.sh

---
+++ runtest.sh
@@ -0,0 +1,10 @@
+echo "Running Test Case Generation..."  
+rm -rf allure-results
+set isodate = $(date '+%Y-%m-%d-%H-%M-%S')
+npx playwright test generate.main.spec.ts
+echo "Running Test Cases..."
+npx playwright test main.spec.runEachTest.ts --reporter=line,allure-playwright 
+echo "Generating Report..."
+npx allure generate ./allure-results --clean -o ./allure-report--$isodate && npx allure open ./allure-report--$isodate
+#To clean all the allure-report folder 
+#rm -Rf allure-report*
